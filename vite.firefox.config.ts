ca30ac914c4f3b7a832ef5efe81095695349894a
Hideki Ikemoto <ikemo333@gmail.com>
2024-03-17 15:04:35 +0900

    Firefox拡張の作成機能を追加

---
+++ vite.firefox.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.firefox.json";
+// import devtools from 'solid-devtools/vite';
+
+export default defineConfig({
+  plugins: [
+    /* 
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
+    // devtools(),
+    solidPlugin(),
+    crx({ manifest, browser: "firefox" }),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "dist/firefox",
+    target: "esnext",
+  },
+});
