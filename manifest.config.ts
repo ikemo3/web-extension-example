3eb7db3c90f5ad09ca6d40266fde72a1f92a277d
Hideki Ikemoto <ikemo333@gmail.com>
2024-03-20 15:13:49 +0900

    マニフェスト作成コードの整理

--- manifest.config.ts
+++ manifest.config.ts
@@ -4,40 +4,33 @@ import packageJson from "./package.json";
 import { convertSemverToManifestVersion } from "./src/libs/version";
 const { version } = packageJson;
 
-const baseManifest = {
-  manifest_version: 3,
-  name: "CRXJS Solid Vite Example",
-  version: convertSemverToManifestVersion(version),
-  action: { default_popup: "index.html" },
-  options_ui: {
-    page: "options.html",
-    open_in_tab: true,
-  },
-  content_scripts: [
-    {
-      js: ["src/content.tsx"],
-      matches: ["https://example.com/*"],
-    },
-  ],
-};
-
-const chromeManifest = baseManifest;
-
-const firefoxManifest = {
-  ...baseManifest,
-  browser_specific_settings: {
-    gecko: {
-      id: "chrome-extension-example@ikemo3.com",
-    },
+const firefoxSpecificSettings = {
+  gecko: {
+    id: "chrome-extension-example@ikemo3.com",
   },
 };
 
 export default defineManifest((env) => {
   const isFirefox = env.mode === "firefox";
+  const browserSpecificSettings = isFirefox
+    ? firefoxSpecificSettings
+    : undefined;
 
-  if (isFirefox) {
-    return firefoxManifest;
-  } else {
-    return chromeManifest;
-  }
+  return {
+    manifest_version: 3,
+    name: "CRXJS Solid Vite Example",
+    version: convertSemverToManifestVersion(version),
+    action: { default_popup: "index.html" },
+    options_ui: {
+      page: "options.html",
+      open_in_tab: true,
+    },
+    content_scripts: [
+      {
+        js: ["src/content.tsx"],
+        matches: ["https://example.com/*"],
+      },
+    ],
+    browser_specific_settings: browserSpecificSettings,
+  };
 });
