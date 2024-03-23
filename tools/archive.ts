8c5948e0b7d6947cce4621f53fe4576cae22bd36
Hideki Ikemoto <ikemo333@gmail.com>
2024-03-23 22:25:21 +0900

    アーカイブ方式の改善
    - アーカイブ前に.vite を削除
    - Firefoxもzip-a-folderでアーカイブする方式に変更

--- tools/archive.ts
+++ tools/archive.ts
@@ -5,4 +5,10 @@ if (!fs.existsSync("./artifacts")) {
   fs.mkdirSync("./artifacts");
 }
 
+// dist/{chrome,firefox}/.vite を削除
+fs.rmdirSync("./dist/chrome/.vite", { recursive: true });
+fs.rmdirSync("./dist/firefox/.vite", { recursive: true });
+
+// dist/{chrome,firefox} を zip にする
 await zip("./dist/chrome", "./artifacts/chrome.zip");
+await zip("./dist/firefox", "./artifacts/firefox.zip");
